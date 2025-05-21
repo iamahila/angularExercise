f7a17b74cb8db77e89976d1dc960627518d19884
iamahila <ahilachockalingam@gmail.com>
2025-05-21 11:41:14 +0530

    Pipes and component interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exercise';
+  name='Ahila Karthick Nila';
+  message: string;
+
+  checkEvent(message: string){
+    this.message = message;
+  }
 }
--- src/todo-table/todo-table.component.ts
+++ src/todo-table/todo-table.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-table',
   templateUrl: './todo-table.component.html',
   styleUrls: ['./todo-table.component.css']
 })
-export class TodoTableComponent {
+export class TodoTableComponent implements OnInit{
 
   
   hasError= false;
   isSpecial = true;
+  greeting = "";
+  name: string = "Ahila Karthick";
+  displayName = false;
+  color = 'yellow';
+  colors: string[] = ['red', 'blue', 'green'];
+  date = new Date();
+  @Input() parentData: string; 
+  @Output() childEvent = new EventEmitter<string>();
   messageClasses = {
     'normal': !this.hasError,
     'error': this.hasError,
@@ -21,6 +29,18 @@ export class TodoTableComponent {
     fontStyle: "italic"
   }
 
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  onClick(){
+    this.greeting = 'Hello';
+    this.childEvent.emit('New event emitted');
+  }
+
+  logMessage(value: string){
+    console.log(value);
+  }
   
 
 }
